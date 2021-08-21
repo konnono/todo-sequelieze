afd67c0c97f547d41ddba0e183d2079bbcff0000
konnono <konnono@gmail.com>
2021-08-21 22:29:10 +0800

    Feat: Project init

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const session = require('express-session')
+const bodyParser = require('./node_modules/body-parser')
+const methodOverride = require('method-override')
+
+const app = express()
+const hbs = exphbs.create({
+  defaultLayout: 'main', extname: '.hbs'
+})
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true
+}))
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is up and running on port:${process.env.PORT}`)
+})
