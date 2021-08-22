ff65b9d712bfd7ed800f49792f051f7dce542478
konnono <konnono@gmail.com>
2021-08-22 15:52:29 +0800

    Feat: Add connect-flash and display message partials

--- app.js
+++ app.js
@@ -3,19 +3,16 @@ const exphbs = require('express-handlebars')
 const session = require('express-session')
 const bodyParser = require('./node_modules/body-parser')
 const methodOverride = require('method-override')
+const flash = require('connect-flash')
 
 const app = express()
-const hbs = exphbs.create({
-  defaultLayout: 'main', extname: '.hbs'
-})
+const routes = require('./routes')
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 
-const routes = require('./routes')
-
-app.engine('hbs', hbs.engine)
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -21,6 +21,24 @@ router.get('/register', (req, res) => {
 
 router.post('/register', (req, res) => {
   const { name, email, password, confirmPassword } = req.body
+
+  const errors = []
+  if (!name || !email || !password || !confirmPassword) {
+    errors.push({ message: '所有欄位都是必填。' })
+  }
+  if (password !== confirmPassword) {
+    errors.push({ message: '密碼與確認密碼不相符！' })
+  }
+  if (errors.length) {
+    return res.render('register', {
+      errors,
+      name,
+      email,
+      password,
+      confirmPassword
+    })
+  }
+
   User.findOne({ where: { email } }).then(user => {
     if (user) {
       console.log('User already exists')
@@ -42,6 +60,12 @@ router.post('/register', (req, res) => {
       .then(() => res.redirect('/'))
       .catch(err => console.log(err))
   })
+})
+
+router.get('/logout', (req, res) => {
+  req.logout()
+  // req.flash('success_msg', '你已經成功登出')
+  res.redirect('/users/login')
 })
 
 module.exports = router
